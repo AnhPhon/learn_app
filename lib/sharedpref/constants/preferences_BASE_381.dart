bdc2a2704e2c156a89268b9c04f73ea304f68d76
Pham Zuong <duongpro113333@gmail.com>
2021-09-25 08:47:22 +0700

    Duong: backup deleted

--- lib/sharedpref/constants/preferences_BASE_381.dart
+++
@@ -1,7 +0,0 @@
-class Preferences {
-  Preferences._();
-
-  static const String jwt_token = "jwt_token";
-  static const String is_dark_mode = "is_dark_mode";
-  static const String current_language = "current_language";
-}
--- lib/sharedpref/constants/preferences_REMOTE_381.dart
+++
@@ -1,9 +0,0 @@
-class Preferences {
-  Preferences._();
-
-  static const String jwt_token = "jwt_token";
-  static const String is_dark_mode = "is_dark_mode";
-  static const String current_language = "current_language";
-  static const String username = "username";
-  static const String password = "password";
-}
