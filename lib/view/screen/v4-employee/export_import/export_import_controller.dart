4d8a350d30cc0c0c4791fd3f6a5d0ac93e6bdc8c
Anh Phon <ngotrananhphon.flutter.dev@gmail.com>
2021-10-07 16:38:23 +0700

    start

---
+++ lib/view/screen/v4-employee/export_import/export_import_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class V4ExportImportControleer extends GetxController {
+//khi báo isLoading
+  bool isLoading = true;
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+}
---
+++ lib/view/screen/v4-employee/export_import/export_import_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:template/view/screen/v4-employee/export_import/export_import_controller.dart';
+
+class V4ExportImportPage extends GetView<V4ExportImportControleer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
