242b446d22bde30876f0ce7385922481b74af4bf
Toan <0352972441>
2021-10-08 08:56:33 +0700

    create branch

---
+++ lib/view/screen/v2-builder/candicate_recruitment/recruitment/recruitment_page.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:template/view/screen/v2-builder/candicate_recruitment/recruitment/recruitment_controller.dart';
+import 'package:template/view/screen/v4-employee/notification/components/appbar_notifcation_page.dart';
+
+class V2RecruitmentPage extends GetView<V2RecruitmentController>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(title: "",),
+    );
+  }
+  
+}
