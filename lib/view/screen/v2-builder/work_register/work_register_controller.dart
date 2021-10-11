c3964a1fd029d349fa5462e813185098278da129
Pham Zuong <duongpro113333@gmail.com>
2021-10-12 06:43:50 +0700

    update

--- lib/view/screen/v2-builder/work_register/work_register_controller.dart
+++ lib/view/screen/v2-builder/work_register/work_register_controller.dart
@@ -2,8 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
-import 'package:template/data/model/v1/select_province_custom_model.dart';
 import 'package:template/routes/app_routes.dart';
+
+class SelectProvinceCustomModel {
+  String value;
+  Function(dynamic) onChanged;
+
+  SelectProvinceCustomModel({required this.value, required this.onChanged});
+}
 
 class V2WorkRegisterController extends GetxController {
   String title = "Đăng ký việc";
