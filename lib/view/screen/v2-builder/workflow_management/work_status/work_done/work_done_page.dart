13639fbceac58a58e5da14dd4862b42220922e09
Anh Phon <ngotrananhphon.flutter.dev@gmail.com>
2021-10-11 08:30:31 +0700

    update

--- lib/view/screen/v2-builder/workflow_management/work_status/work_done/work_done_page.dart
+++ lib/view/screen/v2-builder/workflow_management/work_status/work_done/work_done_page.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:template/view/basewidget/appbar/app_bar_widget.dart';
-import 'package:template/view/screen/v2-builder/workflow_management/workflow_management_controller.dart';
+import 'package:template/view/screen/v2-builder/workflow_management/work_status/work_done/work_done_controller.dart';
 
-//
-/// Lấy V2WorkflowManagementController để dùng Model ở Workflow Management nhằm
-/// mục đích khi click các item tương ứng trong danh sách việc đã làm
-///
-class V2WorkDonePage extends GetView<V2WorkflowManagementController> {
+class V2WorkDonePage extends GetView<V2WorkDoneController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarWidget(
         title: "Việc đã làm",
       ),
-      body: GetBuilder<V2WorkflowManagementController>(
-          init: V2WorkflowManagementController(),
-          builder: (V2WorkflowManagementController controller) {
+      body: GetBuilder<V2WorkDoneController>(
+          init: V2WorkDoneController(),
+          builder: (V2WorkDoneController controller) {
             return Column();
           }),
     );
