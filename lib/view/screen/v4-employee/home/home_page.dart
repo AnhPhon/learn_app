4b656c1338ac5c54d4e50ff3a1110ca5047df478
Pham Zuong <duongpro113333@gmail.com>
2021-10-13 19:21:40 +0700

    update

--- lib/view/screen/v4-employee/home/home_page.dart
+++ lib/view/screen/v4-employee/home/home_page.dart
@@ -21,13 +21,7 @@ class V4HomePage extends GetView<V4HomeController> {
       body: GetBuilder<V4HomeController>(
         init: V4HomeController(),
         builder: (V4HomeController controller) {
-          if (controller.isLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
           return HomeWidget(
-            image: controller.avatar,
             fullname: "Hi, ${controller.fullname}!",
             notificationURL: AppRoutes.V4_NOTIFICATION,
             content: Column(
@@ -104,8 +98,7 @@ class V4HomePage extends GetView<V4HomeController> {
         alignment: Alignment.center,
         decoration: BoxDecoration(
           color: const Color(0xff2196F3),
-          borderRadius:
-              BorderRadius.circular(Dimensions.BORDER_RADIUS_EXTRA_SMALL),
+          borderRadius: BorderRadius.circular(Dimensions.BORDER_RADIUS_EXTRA_SMALL),
         ),
         child: Center(
           child: Text(
@@ -129,8 +122,7 @@ class V4HomePage extends GetView<V4HomeController> {
       padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
       decoration: const BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.all(
-              Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT)),
+          borderRadius: BorderRadius.all(Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT)),
           boxShadow: [
             BoxShadow(offset: Offset(0, 2), color: Colors.grey, blurRadius: 2),
           ]),
@@ -242,8 +234,7 @@ class V4HomePage extends GetView<V4HomeController> {
       padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
       decoration: const BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.all(
-              Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT)),
+          borderRadius: BorderRadius.all(Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT)),
           boxShadow: [
             BoxShadow(offset: Offset(0, 2), color: Colors.grey, blurRadius: 2),
           ]),
@@ -407,8 +398,7 @@ class V4HomePage extends GetView<V4HomeController> {
       padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
       decoration: const BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.all(
-              Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT)),
+          borderRadius: BorderRadius.all(Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT)),
           boxShadow: [
             BoxShadow(offset: Offset(0, 2), color: Colors.grey, blurRadius: 2),
           ]),
