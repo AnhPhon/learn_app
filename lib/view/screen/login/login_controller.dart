31bf2ca04cc4178e2362e1c80da53a66b130219e
Pham Zuong <pduongpdu99@gmail.com>
2021-10-18 08:43:25 +0700

    30 login update id

--- lib/view/screen/login/login_controller.dart
+++ lib/view/screen/login/login_controller.dart
@@ -28,7 +28,7 @@ class LoginController extends GetxController {
   /// on login click
   ///
   void onLoginBtnClick() {
-    sl.get<SharedPreferenceHelper>().saveUserId("6161831d4afc4f67f5e66eaf");
+    sl.get<SharedPreferenceHelper>().saveUserId("616a8b65000118120619d644");
     if (usernameController.text == '1') {
       Get.toNamed(AppRoutes.V1_DASHBOARD);
     } else if (usernameController.text == '2') {
