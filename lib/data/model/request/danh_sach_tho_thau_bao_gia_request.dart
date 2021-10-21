25ecd473466abc891075b6cb2c2e23a22c99f7a0
Your Name <you@example.com>
2021-10-21 20:36:46 +0700

    Tho thau dang ky du an khach hang

--- lib/data/model/request/danh_sach_tho_thau_bao_gia_request.dart
+++ lib/data/model/request/danh_sach_tho_thau_bao_gia_request.dart
@@ -1,3 +1,8 @@
+import 'package:template/data/model/response/loai_cong_viec_response.dart';
+import 'package:template/data/model/response/mat_hang_dac_trung_response.dart';
+import 'package:template/data/model/response/nhom_cua_hang_response.dart';
+import 'package:template/data/model/response/nhom_dich_vu_response.dart';
+
 class DanhSachThoThauBaoGiaRequest {
   String? id;
   String? idDuAnKhachHang;
@@ -9,17 +14,36 @@ class DanhSachThoThauBaoGiaRequest {
   String? tienCoc;
   String? daXem;
 
+  // For thothau
+  String? idNhomDichVu;
+  List<String>? idLoaiCongViecs;
+  String? thoiGianBatDauLam;
+  int? soLuongNguoi;
+
+  // For daily
+  String? idNhomCuaHang;
+  List<String>? idMatHangDacTrungs;
+  String? noiDungMoTa;
+
   DanhSachThoThauBaoGiaRequest({
-      this.id,
-      this.idDuAnKhachHang,
-      this.taiKhoanBaoGia,
-      this.idTrangThaiBaoGia,
-      this.thoiGianHoanThanh,
-      this.giaBao,
-      this.ghiChu,
-      this.tienCoc,
-      this.daXem});
-  
+    this.id,
+    this.idDuAnKhachHang,
+    this.taiKhoanBaoGia,
+    this.idTrangThaiBaoGia,
+    this.thoiGianHoanThanh,
+    this.giaBao,
+    this.ghiChu,
+    this.tienCoc,
+    this.daXem,
+    this.idNhomDichVu,
+    this.idLoaiCongViecs,
+    this.thoiGianBatDauLam,
+    this.soLuongNguoi,
+    this.idNhomCuaHang,
+    this.idMatHangDacTrungs,
+    this.noiDungMoTa,
+  });
+
   ///
   /// From JSON
   ///
@@ -33,6 +57,22 @@ class DanhSachThoThauBaoGiaRequest {
     ghiChu = json['ghiChu'].toString();
     tienCoc = json['tienCoc'].toString();
     daXem = json['daXem'].toString();
+
+    idNhomDichVu = json['idNhomDichVu'].toString();
+    idLoaiCongViecs = [];
+    if(json['idLoaiCongViecs'] != null){
+      json['idLoaiCongViecs'].map((e) => idLoaiCongViecs!.add(e.toString()));
+    }
+    if(json['thoiGianBatDauLam'] != null) thoiGianBatDauLam = json['thoiGianBatDauLam'].toString();
+    if(json['soLuongNguoi'] != null) soLuongNguoi = int.parse(json['soLuongNguoi'].toString());
+
+    idNhomCuaHang = json['idNhomCuaHang'].toString();
+    idMatHangDacTrungs = [];
+    if(json['idMatHangDacTrungs'] != null){
+      json['idMatHangDacTrungs'].map((e) => idMatHangDacTrungs!.add(e.toString()));
+    }
+    noiDungMoTa = json['noiDungMoTa'].toString();
+
   }
 
   ///
@@ -41,32 +81,52 @@ class DanhSachThoThauBaoGiaRequest {
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     // check null id
-    if (id != null) data['id'] = id; 
+    // if (id != null) data['id'] = id;
 
     // check null idDuAnKhachHang
-    if (idDuAnKhachHang != null) data['idDuAnKhachHang'] = idDuAnKhachHang; 
+    if (idDuAnKhachHang != null) data['idDuAnKhachHang'] = idDuAnKhachHang;
 
     // check null taiKhoanBaoGia
-    if (taiKhoanBaoGia != null) data['taiKhoanBaoGia'] = taiKhoanBaoGia; 
+    if (taiKhoanBaoGia != null) data['taiKhoanBaoGia'] = taiKhoanBaoGia;
 
     // check null idTrangThaiBaoGia
-    if (idTrangThaiBaoGia != null) data['idTrangThaiBaoGia'] = idTrangThaiBaoGia; 
+    // if (idTrangThaiBaoGia != null) data['idTrangThaiBaoGia'] = idTrangThaiBaoGia;
 
     // check null thoiGianHoanThanh
-    if (thoiGianHoanThanh != null) data['thoiGianHoanThanh'] = thoiGianHoanThanh; 
+    if (thoiGianHoanThanh != null) data['thoiGianHoanThanh'] = thoiGianHoanThanh;
 
     // check null giaBao
-    if (giaBao != null) data['giaBao'] = giaBao; 
+    if (giaBao != null) data['giaBao'] = giaBao;
 
     // check null ghiChu
-    if (ghiChu != null) data['ghiChu'] = ghiChu; 
+    if (ghiChu != null) data['ghiChu'] = ghiChu;
 
     // check null tienCoc
-    if (tienCoc != null) data['tienCoc'] = tienCoc; 
+    if (tienCoc != null) data['tienCoc'] = tienCoc;
 
     // check null daXem
-    if (daXem != null) data['daXem'] = daXem; 
+    if (daXem != null) data['daXem'] = daXem;
 
+    // check null idNhomDichVu
+    if (idNhomDichVu != null) data['idNhomDichVu'] = idNhomDichVu;
+
+    // check null daXem
+    if (idLoaiCongViecs != null) data['idLoaiCongViecs'] = idLoaiCongViecs;
+
+    // check null daXem
+    if (thoiGianBatDauLam != null) data['thoiGianBatDauLam'] = thoiGianBatDauLam.toString();
+
+    // check null daXem
+    if (soLuongNguoi != null) data['soLuongNguoi'] = soLuongNguoi;
+
+    // check null daXem
+    if (idNhomCuaHang != null) data['idNhomCuaHang'] = idNhomCuaHang;
+
+    // check null daXem
+    if (idMatHangDacTrungs != null) data['idMatHangDacTrungs'] = idMatHangDacTrungs;
+
+    // check null daXem
+    if (noiDungMoTa != null) data['noiDungMoTa'] = noiDungMoTa;
 
     return data;
   }
