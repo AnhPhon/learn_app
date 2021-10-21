f012d5616edecceb8f216ba0803daed6185425ce
Your Name <you@example.com>
2021-10-21 12:33:52 +0700

    Add form dang ky du an

--- lib/view/screen/v2-builder/project/project_dang_ky_trien_khai/project_dang_ky_trien_khai_page.dart
+++ lib/view/screen/v2-builder/project/project_dang_ky_trien_khai/project_dang_ky_trien_khai_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
+import 'package:multi_select_flutter/util/multi_select_list_type.dart';
+import 'package:template/data/model/response/loai_cong_viec_response.dart';
 import 'package:template/data/model/response/nhom_dich_vu_response.dart';
 import 'package:template/helper/date_converter.dart';
 import 'package:template/utils/color_resources.dart';
@@ -7,6 +10,8 @@ import 'package:template/utils/device_utils.dart';
 import 'package:template/utils/dimensions.dart';
 import 'package:template/view/basewidget/appbar/app_bar_widget.dart';
 import 'package:template/view/basewidget/button/drop_down_button.dart';
+import 'package:template/view/basewidget/textfield/text_field_date.dart';
+import 'package:template/view/basewidget/widgets/label.dart';
 import 'package:template/view/screen/v2-builder/component_builder/btn_component.dart';
 import 'package:template/view/screen/v2-builder/project/project_dang_ky_trien_khai/project_dang_ky_trien_khai_controller.dart';
 
@@ -110,72 +115,15 @@ class V2ProjectDangKyTrienKhaiPage extends GetView<V2ProjectDangKyTrienKhaiContr
             height: Dimensions.MARGIN_SIZE_LARGE,
           ),
           Text(
-            "Các hạng mục triển khai: ",
+            "Thợ thầu đăng ký hạng mục tham gia báo giá",
             textAlign: TextAlign.left,
             style: Dimensions.fontSizeStyle18w600(),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Dimensions.PADDING_SIZE_DEFAULT,
-              vertical: Dimensions.PADDING_SIZE_DEFAULT,
-            ),
-            decoration: const BoxDecoration(
-              color: ColorResources.WHITE,
-              borderRadius: BorderRadius.all(
-                Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT),
-              ),
-            ),
-            width: DeviceUtils.getScaledWidth(Get.context!, 1),
-            child: Wrap(
-              children: controller.kiemTraIdHangMucXayDungs()
-                  ? controller.duAnKhachHangResponse!.idHangMucXayDungs!.map((e) {
-                      return Container(
-                        decoration: const BoxDecoration(
-                          color: ColorResources.HOME_BG,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(Dimensions.BORDER_RADIUS_DEFAULT),
-                          ),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: Dimensions.PADDING_SIZE_SMALL,
-                          vertical: Dimensions.PADDING_SIZE_SMALL,
-                        ),
-                        child: Text(
-                          e.tieuDe.toString(),
-                          textAlign: TextAlign.left,
-                          style: const TextStyle(
-                            fontSize: Dimensions.FONT_SIZE_EXTRA_LARGE,
-                          ),
-                        ),
-                      );
-                    }).toList()
-                  : [
-                      const Center(
-                        child: Text(
-                          'Không có',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: Dimensions.FONT_SIZE_EXTRA_LARGE,
-                          ),
-                        ),
-                      ),
-                    ],
-            ),
-          ),
-          const Text(
-            "Nếu thầu thợ có thể tham gia dự án này, hãy kích nút “Đăng ký” chúng tôi sẽ liên hệ với bạn nếu dự án cần thêm nhân lực",
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              color: ColorResources.RED,
-              fontSize: Dimensions.FONT_SIZE_LARGE,
-              fontWeight: FontWeight.w600,
-            ),
           ),
           const SizedBox(
             height: Dimensions.MARGIN_SIZE_LARGE,
           ),
 
-          // hôn nhân
+          // nhom cong viec
           if (controller.nhomDichVuResponse != null && controller.nhomDichVuResponse!.length > 2)
             DropDownButton<NhomDichVuResponse>(
               onChanged: (val) => controller.updateNhomDichVu(val!),
@@ -183,12 +131,107 @@ class V2ProjectDangKyTrienKhaiPage extends GetView<V2ProjectDangKyTrienKhaiContr
               width: DeviceUtils.getScaledWidth(Get.context!, 1),
               value: controller.currentNhomDichVuResponse,
               obligatory: true,
-              label: "Các hạng mục triển khai: ",
+              label: "Chọn nhóm công việc phù hợp",
               hint: controller.currentNhomDichVuResponse.tenDichVu.toString(),
             )
           else
             const SizedBox(),
 
+          const Label(
+            label: "Chọn công việc phù hợp",
+            obligatory: true,
+            horizontalPadding: 0,
+            style: TextStyle(
+              fontSize: Dimensions.FONT_SIZE_LARGE,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          // cong viec phu hop
+          if (controller.nhomDichVuResponse != null && controller.nhomDichVuResponse!.length > 2)
+            MultiSelectDialogField(
+              listType: MultiSelectListType.CHIP,
+              items: controller.loaiCongViecMultiSelectItem,
+              title: const Text("Hãy chọn các công việc phù hợp"),
+              selectedColor: Colors.blue,
+              selectedItemsTextStyle: const TextStyle(color: ColorResources.WHITE),
+              checkColor: ColorResources.WHITE,
+              buttonText: const Text(
+                "Chọn công việc phù hợp",
+                style: TextStyle(
+                  color: ColorResources.GREY,
+                  fontSize: 16,
+                ),
+              ),
+              onConfirm: (List<LoaiCongViecResponse?> results) {
+                controller.currentLoaiCongViecResponseList = results.cast<LoaiCongViecResponse>();
+              },
+            )
+          else
+            const SizedBox(),
+
+          const Label(
+            label: "Thời gian bắt đầu làm",
+            obligatory: true,
+            horizontalPadding: 0,
+            style: TextStyle(
+              fontSize: Dimensions.FONT_SIZE_LARGE,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          TextFieldDate(
+            paddingTop: 0,
+            allowEdit: false,
+            controller: controller.textEditingController,
+            fontSize: Dimensions.FONT_SIZE_EXTRA_LARGE,
+            holdplacer: 'Chọn ngày',
+            // holdplacer: "22-11-2021",
+            // label: "Thời gian kết thúc dự kiến",
+            obligatory: false,
+            typeInput: TextInputType.datetime,
+            width: DeviceUtils.getScaledWidth(Get.context!, 1),
+            isDate: true,
+          ),
+
+          const SizedBox(
+            height: Dimensions.MARGIN_SIZE_LARGE,
+          ),
+          const Label(
+            label: "Số lượng người",
+            obligatory: true,
+            horizontalPadding: 0,
+            style: TextStyle(
+              fontSize: Dimensions.FONT_SIZE_LARGE,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Container(
+            child: TextField(
+              textInputAction: TextInputAction.next,
+              textAlignVertical: TextAlignVertical.center,
+              controller: controller.soLuongNguoiLamController,
+              keyboardType: TextInputType.number,
+              cursorColor: ColorResources.PRIMARY,
+              decoration: InputDecoration(
+                isDense: true,
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: Dimensions.PADDING_SIZE_SMALL,
+                  vertical: Dimensions.PADDING_SIZE_DEFAULT,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(Dimensions.BORDER_RADIUS_EXTRA_SMALL),
+                ),
+                focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(Dimensions.BORDER_RADIUS_DEFAULT), borderSide: const BorderSide(color: ColorResources.PRIMARY)),
+                enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(Dimensions.BORDER_RADIUS_DEFAULT), borderSide: const BorderSide(color: ColorResources.GREY)),
+                disabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(Dimensions.BORDER_RADIUS_DEFAULT), borderSide: const BorderSide(color: ColorResources.GREY)),
+                hintText: "Nhập số lượng người tham gia",
+                filled: true,
+                fillColor: Colors.transparent,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: Dimensions.MARGIN_SIZE_LARGE,
+          ),
           BtnCustom(
             onTap: () {
               controller.moViewDangKyViecMoi();
@@ -196,6 +239,9 @@ class V2ProjectDangKyTrienKhaiPage extends GetView<V2ProjectDangKyTrienKhaiContr
             color: ColorResources.PRIMARY,
             text: "Đăng ký",
             width: DeviceUtils.getScaledWidth(Get.context!, 1),
+          ),
+          const SizedBox(
+            height: Dimensions.MARGIN_SIZE_LARGE,
           ),
         ],
       ),
