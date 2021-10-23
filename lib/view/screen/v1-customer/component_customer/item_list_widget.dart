906e34ba01f55be523b4c1a0876d536d0998274c
DesktopAdmin <desktopAdmin@gmail.com>
2021-10-23 11:12:54 +0700

    phuonglh: add margin to ItemListWidget

--- lib/view/screen/v1-customer/component_customer/item_list_widget.dart
+++ lib/view/screen/v1-customer/component_customer/item_list_widget.dart
@@ -40,7 +40,7 @@ class ItemListWidget extends StatelessWidget {
       onTap: onTap,
       child: Container(
         margin: const EdgeInsets.symmetric(
-          vertical: Dimensions.MARGIN_SIZE_SMALL,
+          vertical: Dimensions.MARGIN_SIZE_EXTRA_SMALL,
         ),
         height: DeviceUtils.getScaledHeight(context, .13),
         decoration: BoxDecoration(
