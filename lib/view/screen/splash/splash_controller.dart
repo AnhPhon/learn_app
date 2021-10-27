51df457631bd2afe58b66839b773947287604252
Toan <0352972441>
2021-10-27 18:00:23 +0700

    add double back

--- lib/view/screen/splash/splash_controller.dart
+++ lib/view/screen/splash/splash_controller.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:template/di_container.dart';
 import 'package:template/routes/app_routes.dart';
 import 'package:template/sharedpref/shared_preference_helper.dart';
+import 'package:template/utils/alert.dart';
+import 'package:template/utils/app_constants.dart';
 
 class SplashController extends GetxController
     with SingleGetTickerProviderMixin {
@@ -18,17 +21,39 @@ class SplashController extends GetxController
     _animationController!.forward().whenComplete(() async {
       final bool? isFirst = await sl.get<SharedPreferenceHelper>().isFirst;
       final bool? isLogin = await sl.get<SharedPreferenceHelper>().isLogin;
+      final String? idLoaiTaiKhoan = await sl.get<SharedPreferenceHelper>().typeAccount;
 
       // is first time
       if (isFirst == null) {
-        Get.offNamed(AppRoutes.INTRO);
+        Get.offAllNamed(AppRoutes.INTRO);
       } else {
         // check if is login
         if (isLogin != null && isLogin) {
-          Get.offNamed(AppRoutes.DASHBOARD);
+          if(idLoaiTaiKhoan != null){
+            if (idLoaiTaiKhoan == KHACH_HANG) {
+              Get.offAndToNamed(AppRoutes.V1_DASHBOARD);
+              return;
+            } else if (idLoaiTaiKhoan == THO_THAU) {
+              Get.offAndToNamed(AppRoutes.V2_DASHBOARD);
+              return;   
+            } else if (idLoaiTaiKhoan == DAI_LY) {
+              Get.offAndToNamed(AppRoutes.V3_DASHBOARD);
+              return;
+            } else if (idLoaiTaiKhoan == NHAN_VIEN) {
+              Get.offAndToNamed(AppRoutes.V4_DASHBOARD);
+              return;
+            }else{
+              // Logout
+              update();
+            }
+          }else{
+            // logout
+            update();
+          }
+          //Get.offNamed(AppRoutes.DASHBOARD);
         } else {
           // is not login
-          Get.offNamed(AppRoutes.LOGIN);
+          Get.offAllNamed(AppRoutes.LOGIN);
         }
       }
     });
--- lib/view/screen/v1-customer/dashboard/dashboard_page.dart
+++ lib/view/screen/v1-customer/dashboard/dashboard_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:template/helper/common_helper.dart';
 import 'package:template/utils/color_resources.dart';
 import 'package:template/view/screen/v1-customer/account/account_page.dart';
 import 'package:template/view/screen/v1-customer/entertainment/entertainment_page.dart';
@@ -19,48 +20,55 @@ class V1DashboardPage extends StatelessWidget {
     return GetBuilder<V1DashboardController>(
       init: V1DashboardController(),
       builder: (controller) {
-        return Scaffold(
-          body: IndexedStack(
-            index: controller.tabIndex,
-            children: [
-              V1HomePage(),
-              V1ProjectPage(),
-              V1EntertainmentPage(),
-              V1NewsPage(),
-              V1AccountPage(),
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            selectedItemColor: ColorResources.PRIMARY,
-            backgroundColor: ColorResources.WHITE,
-            onTap: controller.changeTabIndex,
-            currentIndex: controller.tabIndex,
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: true,
-            showUnselectedLabels: true,
-            items: [
-              _bottomNavigationBarItem(
-                icon: CupertinoIcons.home,
-                label: 'Trang chủ',
-              ),
-              _bottomNavigationBarItem(
-                icon: CupertinoIcons.bag_fill,
-                label: 'Dự án',
-              ),
-              _bottomNavigationBarItem(
-                icon: CupertinoIcons.star_lefthalf_fill,
-                label: 'Giải trí',
-              ),
-              _bottomNavigationBarItem(
-                icon: CupertinoIcons.news,
-                label: 'Tin tức',
-              ),
-              _bottomNavigationBarItem(
-                icon: CupertinoIcons.person,
-                label: 'Tài khoản',
-              ),
-            ],
-          ),
+        return Builder(
+          builder: (context) {
+            return WillPopScope(
+              onWillPop: ()=> controller.onDoubleBack(),
+              child: Scaffold(
+                body: IndexedStack(
+                  index: controller.tabIndex,
+                  children: [
+                    V1HomePage(),
+                    V1ProjectPage(),
+                    V1EntertainmentPage(),
+                    V1NewsPage(),
+                    V1AccountPage(),
+                  ],
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                  selectedItemColor: ColorResources.PRIMARY,
+                  backgroundColor: ColorResources.WHITE,
+                  onTap: controller.changeTabIndex,
+                  currentIndex: controller.tabIndex,
+                  type: BottomNavigationBarType.fixed,
+                  showSelectedLabels: true,
+                  showUnselectedLabels: true,
+                  items: [
+                    _bottomNavigationBarItem(
+                      icon: CupertinoIcons.home,
+                      label: 'Trang chủ',
+                    ),
+                    _bottomNavigationBarItem(
+                      icon: CupertinoIcons.bag_fill,
+                      label: 'Dự án',
+                    ),
+                    _bottomNavigationBarItem(
+                      icon: CupertinoIcons.star_lefthalf_fill,
+                      label: 'Giải trí',
+                    ),
+                    _bottomNavigationBarItem(
+                      icon: CupertinoIcons.news,
+                      label: 'Tin tức',
+                    ),
+                    _bottomNavigationBarItem(
+                      icon: CupertinoIcons.person,
+                      label: 'Tài khoản',
+                    ),
+                  ],
+                ),
+              ), 
+            );
+          },
         );
       },
     );
