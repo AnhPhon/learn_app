51d44e8e580474f0a0d4bafaa3ca5438d75d8fbd
DesktopAdmin <desktopAdmin@gmail.com>
2021-10-27 09:05:18 +0700

    phuonglh fix bug merge source

--- lib/view/screen/login/login_controller.dart
+++ lib/view/screen/login/login_controller.dart
@@ -55,9 +55,7 @@ class LoginController extends GetxController {
   /// on login click
   ///
   void onLoginBtnClick() {
-    sl.get<SharedPreferenceHelper>().saveUserId("616d7c0dc48e736e333ef26b");
-    // sl.get<SharedPreferenceHelper>().saveUserId("616d99dd7e28e22b158543bb");
-
+    sl.get<SharedPreferenceHelper>().saveUserId("616d99dd7e28e22b158543bb"); 
     if (usernameController.text == '1') {
       Get.toNamed(AppRoutes.V1_DASHBOARD);
     } else if (usernameController.text == '2') {
@@ -65,6 +63,7 @@ class LoginController extends GetxController {
     } else if (usernameController.text == '3') {
       Get.toNamed(AppRoutes.V3_DASHBOARD);
     } else if (usernameController.text == '4') {
+      sl.get<SharedPreferenceHelper>().saveUserId("616d7c0dc48e736e333ef26b");
       Get.toNamed(AppRoutes.V4_DASHBOARD);
     } else {}
 
